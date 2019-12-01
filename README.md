45b429d37ec4c7b9d704ec53265b56bd56e91ee6
flopreynat <flopreynat@protonmail.com>
2019-12-01 20:35:13 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+php-server
+==========
+
+Spin up a php server locally in no time
+
+Read all about what's behind all this here: 
---
+++ includes/content.php
@@ -0,0 +1,7 @@
+<h1><?php echo "Hello, World!";?></h1>
+
+<p>To easily (and quickly) spin up a php server locally, open your terminal, navigate to your folder of choice, and simply run:</p>
+<pre>
+	php -S localhost:2222
+</pre>
+<h3>The End.</h3>
